c3449a5e31d599bb7d48668ef9735385410a6d32
claudio-dalicandro <claudio@dalicandro.me>
2017-10-01 16:55:49 +0200

    Edeliver configuration

--- config/prod.exs
+++ config/prod.exs
@@ -1,9 +1,5 @@
 use Mix.Config
 
 config :katabot, :webhook,
-  protocol: :https,
-  options: [port: 8080,
-            otp_app: :katabot,
-            keyfile: "priv/ssl/private.key",
-            certfile: "priv/ssl/pubkey.pem"]
-
+       protocol: :http,
+       options: [port: 8080]
